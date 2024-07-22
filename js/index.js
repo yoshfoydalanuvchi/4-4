f619bd2e8df3d0888f3d7058279d4a73d26d0552
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-22 19:46:42 +0500

    none

--- js/index.js
+++ js/index.js
@@ -46,44 +46,86 @@
 // console.log(max);
 // console.log(min);
 
+//9
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 10
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// let res = sum / 100;
+// console.log(Math.trunc(res));
+
+//11
+// let num = prompt("Raqam kiriting:");
+// console.log(num.length);
+
+//12
+// let num = +prompt("son kiriting:");
+
+// for (let i = 0; i >= -num; i--) {
+//   console.log(i);
+// }
+
+// 14
+// let sum = 0;
+// for (let i = 1; i <= 200; i++) {
+//   if (i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+// 15
+// let sum = 1;
+// for (let i = 2; i < 20; i++) {
+//   console.log(`(${sum} + ${i}) = ${sum + i}`);
+//   sum = i;
+// }
+
 // Break/continue ga oid masalalar.
 
-//1
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-  if (i === 5) {
-    break;
-  }
-}
+// //1
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+//   if (i === 5) {
+//     break;
+//   }
+// }
 
-//2
-for (let i = 1; i <= 10; i++) {
-  if (i === 3 || i === 7) {
-    continue;
-  }
-  console.log(i);
-}
-//3
-for (let i = 1; i <= 10; i++) {
-  let a = +prompt("Son kiriting:");
-  if (a == 7) {
-    console.log("Topildi!");
-    break;
-  }
-}
+// //2
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 3 || i === 7) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// //3
+// for (let i = 1; i <= 10; i++) {
+//   let a = +prompt("Son kiriting:");
+//   if (a == 7) {
+//     console.log("Topildi!");
+//     break;
+//   }
+// }
 
-//4
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    continue;
-  }
-  console.log(i);
-}
+// //4
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
 
-//5
-for (let i = 1; i <= 100; i++) {
-  console.log(i);
-  if (i > 50) {
-    break;
-  }
-}
+// //5
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+//   if (i > 50) {
+//     break;
+//   }
+// }
