09a1529354c02bc856748d5ac42545561f5911a9
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-07-22 18:02:49 +0500

    none

--- js/index.js
+++ js/index.js
@@ -1,237 +1,89 @@
-// 100% qilib qoyildi
-// If ga oid masalalar
-
-a = 20;
-let res;
-/*
-//1
-if (a >= 18) {
-  res = "Siz katta yoshdagisiz";
-} else {
-  res = "Siz hali yoshsiz";
-}
-console.log(res);
-
-//2
-if (a % 2 == 0) {
-  res = "juft";
-} else {
-  res = "toq";
-}
-console.log(res);
-
-//3
-if (a > 0) {
-  res = "Musbat";
-} else if (a != 0) {
-  res = "Manfiy";
-} else {
-  res = "Nolga teng";
-}
-console.log(res);
-
-//4
-if (a > 20) {
-  res = "Yengil kiying";
-} else {
-  res = "Qalin kiying";
-}
-console.log(res);
-
-//5
-if (a >= 90) {
-  res = "A'lo";
-} else if (a >= 70) {
-  res = "Yaxshi";
-} else if (a >= 50) {
-  res = "Qoniqarli";
-} else {
-  res = "Yomon";
-}
-console.log(res);
-
-//6
-name = "Ali";
-
-if (name == "Ali") {
-  res = "Salom, Ali!";
-} else {
-  res = "Salom, mehmon!";
-}
-console.log(res);
-
-//7
-if (a > 100) {
-  res = "Chegirma qo'llanildi";
-} else {
-  res = "Chegirma mavjud emas";
-}
-console.log(res);
-*/
-
-// Ternary ga oid masalalar
-/*
-//1
-
-res = 25 < a ? "Issiq" : "Sovuq";
-console.log(res);
-
-//2
-res = 50 <= a ? "O'tdi" : "Yiqildi";
-console.log(res);
-
-//3
-res = 0 < a ? "Musbat" : "Manfiy";
-console.log(res);
-*/
-
-// For ga oid masalalar
-
-// //1
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
-
-// //2
-// for (let i = 10; i >= 1; i--) {
-//   console.log(i);
-// }
-
-// //3
-// for (let i = 2; i <= 100; i += 2) {
-//   console.log(i);
-// }
-
-// //4
-// for (let i = 1; i <= 100; i += 2) {
-//   console.log(i);
-// }
-
-// //5
-// let sum = 0;
-// for (let i = 1; i <= 10; i++) {
-//   sum += i;
-// }
-// console.log(sum);
-
-// //6
-// sum = 1;
-// for (let i = 1; i <= 10; i++) {
-//   sum *= i;
-// }
-// console.log(sum);
-
-// //7
-// sum = 1;
-// for (let i = 1; i <= a; i++) {
-//   sum *= i;
-// }
-// console.log(sum);
-
-// //8
-// for (let i = 1; i <= 50; i++) {
-//   console.log(i * i);
-// }
-
-// //9
-// for (let i = 1; i <= 20; i++) {
-//   console.log(i * i * i);
-// }
-
-// //10
-// let sum = 0;
-// for (let i = 1; i <= 1000; i++) {
-//   sum += i;
-// }
-// console.log(sum);
-
-// //11
-// for (let i = 0; i < 5; i++) {
-//   console.log("Salom");
-// }
-
-// //12
-// for (let i = 1; i <= 100; i++) {
-//   console.log(i * 2);
-// }
-
-// //13
-// for (let i = 1; i <= 10; i++) {
-//   console.log(Math.pow(2, i));
-// }
-
-// //14
-
-// for (let i = 1; i <= a; i++) {
-//   console.log(i * i);
-// }
-
-//15
+//15 Oldingi Vazifada qolib ketgandi
 
 // for (let i = 97; i <= 122; i++) {
 //   console.log(String.fromCharCode(i));
 // }
 
-//Boolean ga oid masalalar
-/*
+//For ga oid masalalar
+
 //1
-let b = 10;
-let d = 12;
-if ((b % 2 == 0 && d % 2 == 0) || (b % 2 != 0 && d % 2 != 0)) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
-}
-console.log(res);
+
+// let sum = 0;
+// for (let i = 11; i <= 100; i += 2) {
+//   sum += i;
+// }
+// console.log(sum);
 
 //2
-let c = 11;
-
-if (b > 0 && d > 0 && c > 0) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
-}
-console.log(res);
-
-//3
-if (b > 0 || d > 0 || c > 0) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
-}
-console.log(res);
+// let num = 10;
+// let result = 1;
+// for (let i = 1; i <= num; i++) {
+//   result *= i;
+// }
+// console.log(result);
 
 //4
-if (
-  (b > 0 && d <= 0 && c <= 0) ||
-  (b <= 0 && d > 0 && c <= 0) ||
-  (b <= 0 && d <= 0 && c > 0)
-) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
-}
-console.log(res);
+// let sum = 1;
+// for (let i = 1; i <= 20; i++) {
+//   sum = i * i;
+// }
+// console.log(sum);
 
 //5
-if (
-  (b > 0 && d > 0 && c <= 0) ||
-  (b > 0 && d <= 0 && c > 0) ||
-  (b <= 0 && d > 0 && c > 0)
-) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
-}
-console.log(res);
 
-//6
+// let max = 0;
+// let min = 0;
 
-if (a >= 10 && a <= 99 && a % 2 === 0) {
-  res = "To`g`ri";
-} else {
-  res = "xato";
+// for (let i = 0; i < 5; i++) {
+//   let num = +prompt("sonni kiriting");
+//   if (i == 0 || num < min) {
+//     min = num;
+//   }
+//   if (num > max) {
+//     max = num;
+//   }
+// }
+// console.log(max);
+// console.log(min);
+
+// Break/continue ga oid masalalar.
+
+//1
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i === 5) {
+    break;
+  }
 }
-console.log(res);
-*/
+
+//2
+for (let i = 1; i <= 10; i++) {
+  if (i === 3 || i === 7) {
+    continue;
+  }
+  console.log(i);
+}
+//3
+for (let i = 1; i <= 10; i++) {
+  let a = +prompt("Son kiriting:");
+  if (a == 7) {
+    console.log("Topildi!");
+    break;
+  }
+}
+
+//4
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+//5
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i > 50) {
+    break;
+  }
+}
